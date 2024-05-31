02e007b0a318873782707d777d2d2e2a15e4e071
Mats Vanspringel <mats.vanspringel2006@gmail.com>
2024-05-31 19:16:06 +0200

    modified:   index.html 	modified:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,30 @@
+//Define variables
+dropped = false;
+
+function checkWidth(){
+    //define screen width
+    var screenWidth = screen.width;
+
+    if(screenWidth <= 875){
+        //put service blocks underneath eacht other
+        document.getElementById('serviceBlocks').style.flexDirection = "column";
+
+        //collapse navbar
+        document.getElementById('collapseList').classList.add('droppedDown');
+        document.getElementById('navCollapse').style.display = "block";
+    }
+}
+
+function dropdown(){
+    //check if already dropped
+    if(!dropped){
+        //show dropped list
+        document.getElementById('collapseList').style.display = "block";
+        dropped = true;
+    }
+    else{
+        //hide dropped list
+        document.getElementById('collapseList').style.display = "none";
+        dropped = false;
+    }
+}
